0776db0255e51925047830d57c6b98c9cf8a628f
Pietari Kaskela <pietari.kaskela@gmail.com>
2018-11-23 22:14:25 +0200

    heatmaps working

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+
+export const addressPoints = [
+[61.492635, 23.763434, "571"],
+[61.492625, 23.763434, "486"],
+[61.49270,23.76039, "100010"],
+]
 
 const CustomMap = () => {
   const position = [61.492625, 23.763434]
@@ -9,6 +16,11 @@ const CustomMap = () => {
         url="http://{s}.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
       />
+      <HeatmapLayer
+            points={addressPoints}
+            longitudeExtractor={m => m[1]}
+            latitudeExtractor={m => m[0]}
+            intensityExtractor={m => parseFloat(m[2])} />
       <Marker position={position}>
         <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
       </Marker>
