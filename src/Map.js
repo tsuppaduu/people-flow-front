0e6dbce6028fbbf549817e177634a3488c1e09de
Otto Laitinen <otto.laitinen@hotmail.fi>
2018-11-23 22:06:42 +0200

    Changed zoom level

--- src/Map.js
+++ src/Map.js
@@ -4,7 +4,7 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 const CustomMap = () => {
   const position = [61.492625, 23.763434]
   return (
-    <Map center={position} zoom={13}>
+    <Map center={position} zoom={17}>
       <TileLayer
         url="http://{s}.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
